43473c0f941d328884806b59d6b4d5f8e9df501a
Niranjan Raje <raje.niranjan18@siesgst.ac.in>
2021-03-20 15:25:12 +0530

    Hooks added

--- src/Components/Tasks.jsx
+++ src/Components/Tasks.jsx
@@ -6,12 +6,16 @@ import uncheckImg from './images/notchecked.png'
 const Task = ()=>{
     let [tasks,setTasks] = useState([]);
     let [idCounter,setID] = useState(0);
+    let [percentage,setpercentage] = useState(0);
+
+    // give an unique id to each item
 
     const generateId = ()=>{
         setID(idCounter+1);
         return idCounter;
     }
 
+    // add a task via input
 
     const AddTask = ()=>{
         let taskName = document.querySelector('.add-task');
@@ -28,11 +32,15 @@ const Task = ()=>{
             )
         }
     }
+    
+    // if user deletes a task
 
     const deleteTask = (id)=>{
         const UpdatedTasks = tasks.filter((TaskObj)=>TaskObj.id !== id)
         setTasks(UpdatedTasks)
     }
+
+    // if user completes the task
 
     const CompleteTask=(id)=>{
         setTasks(
@@ -45,11 +53,9 @@ const Task = ()=>{
                                 checked:0
                             }
                         }
-                        else{
-                            return{
-                                ...item,
-                                checked:1
-                            }
+                        return{
+                            ...item,
+                            checked:1
                         }
                     }
                     return item;
@@ -61,7 +67,7 @@ const Task = ()=>{
     if(tasks.length === 0){
         return(
             <>
-            <div className="container-fluid px-5 task-wrap" id="todo">
+            <div className="container-fluid mt-5 px-5 task-wrap mt-5" id="todo">
                 <div className="greet mb-5 mt-4">
                     <h1>All Tasks</h1>
                     <h5 className="greetuser"></h5>
@@ -82,10 +88,14 @@ const Task = ()=>{
     }
     return(
         <>
-            <div className="container-fluid task-wrap">
+            <div className="container-fluid task-wrap mt-5">
                 <div className="greet mb-5 mt-4">
                     <h1>All Tasks</h1>
                     <h5 className="greetuser"></h5>
+                    <div className="percent" style={{color:"white"}}>
+                        <progress value={percentage} max="100" id="progress"></progress>
+                        {percentage}
+                    </div>
                 </div>
                 <div className="container-fluid ">
                     <div className="tasks">
